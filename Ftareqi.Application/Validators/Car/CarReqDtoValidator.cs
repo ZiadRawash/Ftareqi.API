749c0903156d56109183d3628833f42c92ddaa75
Ziad Rawash <ZiadRawash524@gmail.com>
2026-01-18 21:19:34 +0200

    fix(validation): correct fluent validation rules for driver-profile and car

--- Ftareqi.Application/Validators/Car/CarReqDtoValidator.cs
+++ Ftareqi.Application/Validators/Car/CarReqDtoValidator.cs
@@ -9,50 +9,96 @@ namespace Ftareqi.Application.Validators.Car
 	{
 		public CarReqDtoValidator()
 		{
-
+			// Number of seats
 			RuleFor(x => x.NumOfSeats)
 				.NotEmpty().WithMessage("Number of seats is required.")
 				.GreaterThan(0).WithMessage("Seats must be greater than 0.")
 				.LessThanOrEqualTo(5).WithMessage("Maximum allowed seats is 5.");
 
+			// Plate
 			RuleFor(x => x.Plate)
 				.NotEmpty().WithMessage("Plate number is required.")
 				.MinimumLength(3).WithMessage("Plate number is too short.")
 				.MaximumLength(15).WithMessage("Plate number is too long.");
 
+			// Model
 			RuleFor(x => x.Model)
 				.NotEmpty().WithMessage("Model is required.")
-				.MinimumLength(2)
-				.MaximumLength(30);
+				.MinimumLength(2).WithMessage("Model is too short.")
+				.MaximumLength(30).WithMessage("Model is too long.");
 
+			// Color
 			RuleFor(x => x.Color)
 				.NotEmpty().WithMessage("Color is required.")
-				.MaximumLength(30);
+				.MaximumLength(30).WithMessage("Color is too long.");
 
+			// Car Photo
 			RuleFor(x => x.CarPhoto)
 				.NotNull().WithMessage("Car photo is required.")
-				.Must(f => f!.Length > 0).WithMessage("Car photo cannot be empty.")
-				.Must(ImageValidator.BeValidExtension).WithMessage("Car photo type is not allowed.")
-				.Must(ImageValidator.BeValidMimeType).WithMessage("Car photo type is invalid.")
-				.Must(f => f!.Length <= ImageValidator.MaxFileSize).WithMessage("Car photo exceeds file size limit (5MB).");
+				.DependentRules(() =>
+				{
+					RuleFor(x => x.CarPhoto!.Length)
+						.GreaterThan(0).WithMessage("Car photo cannot be empty");
 
+					RuleFor(x => x.CarPhoto!)
+						.Must(ImageValidator.BeValidExtension)
+						.WithMessage("Car photo type is not allowed");
+
+					RuleFor(x => x.CarPhoto!)
+						.Must(ImageValidator.BeValidMimeType)
+						.WithMessage("Car photo type is invalid");
+
+					RuleFor(x => x.CarPhoto!.Length)
+						.LessThanOrEqualTo(ImageValidator.MaxFileSize)
+						.WithMessage("Car photo exceeds file size limit (5MB)");
+				});
+
+			// Car License Front
 			RuleFor(x => x.CarLicenseFront)
 				.NotNull().WithMessage("Front car license photo is required.")
-				.Must(f => f!.Length > 0).WithMessage("Front car license photo cannot be empty.")
-				.Must(ImageValidator.BeValidExtension).WithMessage("Front license type is not allowed.")
-				.Must(ImageValidator.BeValidMimeType).WithMessage("Front license type is invalid.")
-				.Must(f => f!.Length <= ImageValidator.MaxFileSize).WithMessage("Front license photo exceeds size limit (5MB).");
+				.DependentRules(() =>
+				{
+					RuleFor(x => x.CarLicenseFront!.Length)
+						.GreaterThan(0).WithMessage("Front car license photo cannot be empty");
 
+					RuleFor(x => x.CarLicenseFront!)
+						.Must(ImageValidator.BeValidExtension)
+						.WithMessage("Front license type is not allowed");
+
+					RuleFor(x => x.CarLicenseFront!)
+						.Must(ImageValidator.BeValidMimeType)
+						.WithMessage("Front license type is invalid");
+
+					RuleFor(x => x.CarLicenseFront!.Length)
+						.LessThanOrEqualTo(ImageValidator.MaxFileSize)
+						.WithMessage("Front license photo exceeds size limit (5MB)");
+				});
+
+			// Car License Back
 			RuleFor(x => x.CarLicenseBack)
 				.NotNull().WithMessage("Back car license photo is required.")
-				.Must(f => f!.Length > 0).WithMessage("Back car license photo cannot be empty.")
-				.Must(ImageValidator.BeValidExtension).WithMessage("Back license type is not allowed.")
-				.Must(ImageValidator.BeValidMimeType).WithMessage("Back license type is invalid.")
-				.Must(f => f!.Length <= ImageValidator.MaxFileSize).WithMessage("Back license photo exceeds size limit (5MB).");
+				.DependentRules(() =>
+				{
+					RuleFor(x => x.CarLicenseBack!.Length)
+						.GreaterThan(0).WithMessage("Back car license photo cannot be empty");
 
+					RuleFor(x => x.CarLicenseBack!)
+						.Must(ImageValidator.BeValidExtension)
+						.WithMessage("Back license type is not allowed");
+
+					RuleFor(x => x.CarLicenseBack!)
+						.Must(ImageValidator.BeValidMimeType)
+						.WithMessage("Back license type is invalid");
+
+					RuleFor(x => x.CarLicenseBack!.Length)
+						.LessThanOrEqualTo(ImageValidator.MaxFileSize)
+						.WithMessage("Back license photo exceeds size limit (5MB)");
+				});
+
+			// License expiry date
 			RuleFor(x => x.LicenseExpiryDate)
 				.GreaterThan(DateTime.UtcNow.Date.AddDays(1))
-				.WithMessage("License expiry date must be in the future.");
+				.WithMessage("License expiry date must be in the future");
 		}
 	}
 }
--- Ftareqi.Application/Validators/Driver/DriverProfileReqDtoValidator.cs
+++ Ftareqi.Application/Validators/Driver/DriverProfileReqDtoValidator.cs
@@ -17,31 +17,74 @@ namespace Ftareqi.Application.Validators.Driver
 
 		public DriverProfileReqDtoValidator()
 		{
+			// Driver Profile Photo
 			RuleFor(x => x.DriverProfilePhoto)
 				.NotNull().WithMessage("Driver photo is required.")
-				.Must(f => f!.Length > 0).WithMessage("Driver photo cannot be empty.")
-				.Must(ImageValidator.BeValidExtension).WithMessage("Driver photo type is not allowed.")
-				.Must(ImageValidator.BeValidMimeType).WithMessage("Driver photo type is invalid.")
-				.Must(f => f!.Length <= ImageValidator.MaxFileSize).WithMessage("Driver photo exceeds file size limit (5MB).");
+				.DependentRules(() =>
+				{
+					RuleFor(x => x.DriverProfilePhoto!.Length)
+						.GreaterThan(0).WithMessage("Driver photo cannot be empty");
 
+					RuleFor(x => x.DriverProfilePhoto!)
+						.Must(ImageValidator.BeValidExtension)
+						.WithMessage("Driver photo type is not allowed");
+
+					RuleFor(x => x.DriverProfilePhoto!)
+						.Must(ImageValidator.BeValidMimeType)
+						.WithMessage("Driver photo type is invalid");
+
+					RuleFor(x => x.DriverProfilePhoto!.Length)
+						.LessThanOrEqualTo(ImageValidator.MaxFileSize)
+						.WithMessage("Driver photo exceeds file size limit (5MB)");
+				});
+
+			// Driver License Front
 			RuleFor(x => x.DriverLicenseFront)
-				.NotNull().WithMessage("Driver photo is required.")
-				.Must(f => f!.Length > 0).WithMessage("Driver photo cannot be empty.")
-				.Must(ImageValidator.BeValidExtension).WithMessage("Driver photo type is not allowed.")
-				.Must(ImageValidator.BeValidMimeType).WithMessage("Driver photo type is invalid.")
-				.Must(f => f!.Length <= ImageValidator.MaxFileSize).WithMessage("Driver photo exceeds file size limit (5MB).");
+				.NotNull().WithMessage("Driver license front photo is required.")
+				.DependentRules(() =>
+				{
+					RuleFor(x => x.DriverLicenseFront!.Length)
+						.GreaterThan(0).WithMessage("Driver license front cannot be empty");
 
+					RuleFor(x => x.DriverLicenseFront!)
+						.Must(ImageValidator.BeValidExtension)
+						.WithMessage("Driver license front type is not allowed");
+
+					RuleFor(x => x.DriverLicenseFront!)
+						.Must(ImageValidator.BeValidMimeType)
+						.WithMessage("Driver license front type is invalid");
+
+					RuleFor(x => x.DriverLicenseFront!.Length)
+						.LessThanOrEqualTo(ImageValidator.MaxFileSize)
+						.WithMessage("Driver license front exceeds file size limit (5MB)");
+				});
+
+			// Driver License Back
 			RuleFor(x => x.DriverLicenseBack)
-				.NotNull().WithMessage("Driver photo is required.")
-				.Must(f => f!.Length > 0).WithMessage("Driver photo cannot be empty.")
-				.Must(ImageValidator.BeValidExtension).WithMessage("Driver photo type is not allowed.")
-				.Must(ImageValidator.BeValidMimeType).WithMessage("Driver photo type is invalid.")
-				.Must(f => f!.Length <= ImageValidator.MaxFileSize).WithMessage("Driver photo exceeds file size limit (5MB).");
+				.NotNull().WithMessage("Driver license back photo is required.")
+				.DependentRules(() =>
+				{
+					RuleFor(x => x.DriverLicenseBack!.Length)
+						.GreaterThan(0).WithMessage("Driver license back cannot be empty");
 
+					RuleFor(x => x.DriverLicenseBack!)
+						.Must(ImageValidator.BeValidExtension)
+						.WithMessage("Driver license back type is not allowed");
 
+					RuleFor(x => x.DriverLicenseBack!)
+						.Must(ImageValidator.BeValidMimeType)
+						.WithMessage("Driver license back type is invalid");
+
+					RuleFor(x => x.DriverLicenseBack!.Length)
+						.LessThanOrEqualTo(ImageValidator.MaxFileSize)
+						.WithMessage("Driver license back exceeds file size limit (5MB)");
+				});
+
+			// License expiry date
 			RuleFor(x => x.LicenseExpiryDate)
 				.GreaterThan(DateTime.UtcNow.Date.AddDays(1))
-				.WithMessage("License expiry date must be in the future.");
+				.WithMessage("License expiry date must be in the future");
 		}
+
 	}
 }
